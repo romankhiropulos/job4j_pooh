43310ef690fc685d9ccb1f5ec1e0841326ae50cb
romankhiropulos <romankhiropulos@gmail.com>
2022-01-15 20:39:34 +0300

    first commit

---
+++ src/test/java/ru/job4j/pooh/QueueServiceTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.pooh;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QueueServiceTest {
+
+    @Test
+    public void process() {
+    }
+}
---
+++ src/test/java/ru/job4j/pooh/TopicServiceTest.java
@@ -0,0 +1,7 @@
+package ru.job4j.pooh;
+
+import static org.junit.Assert.*;
+
+public class TopicServiceTest {
+
+}
