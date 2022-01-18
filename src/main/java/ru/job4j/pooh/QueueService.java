2ac8c520fa06e829a8dba96a3e4df0e802b64e37
romankhiropulos <romankhiropulos@gmail.com>
2022-01-18 21:19:26 +0300

    QueueService class for poohJMS done!

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,9 +1,30 @@
 package ru.job4j.pooh;
 
+import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class QueueService implements Service {
+
+    private final ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
 
     @Override
     public Resp process(Req req) {
-        return null;
+        String text;
+        Resp resp;
+        switch (req.httpRequestType()) {
+            case "GET" -> {
+                text = queue.poll();
+                resp = Objects.equals(text, "") ? new Resp(text, "204 No Content")
+                        : new Resp(text, "200 OK");
+            }
+            case "POST" -> {
+                queue.add(req.getParam());
+                resp = new Resp("New param inserted to queue!", "200 OK");
+            }
+            default -> {
+                resp = new Resp("Not Found!", "404, Not Found");
+            }
+        }
+        return resp;
     }
 }
